d0fc6bbd9509a33370721418b7b30d860890dc99
Ral-Fireball <99501983+Ral-Fireball@users.noreply.github.com>
2022-12-30 15:21:07 +0100

    Werkende NO2 sensor
    code voor NO2 sensor, werkt, we krijgen volgensmij goede data (hij geeft 0,02 als no2 concentratie, dat klopt moet ons onderzoek).
    NOOT: voor de vorige commit heb ik de barometer werkend gekregen door in de library het I2C te hardcoden.

---
+++ NO2_sensor/NO2_sensor/NO2_sensor.ino
@@ -0,0 +1,29 @@
+const float max_volts = 5.0;
+const float max_analog_steps = 1023.0;
+
+void setup() 
+{
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("Setup initializing");
+
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+}
+
+void loop() 
+{
+  int a0_read = analogRead(A0);
+  int a1_read = analogRead(A1);
+  int a2_read = analogRead(A2);
+  Serial.print("CO (a0): ");
+  Serial.println(a0_read * (max_volts / max_analog_steps));
+  Serial.print("NH3 (a1): ");
+  Serial.println(a1_read * (max_volts / max_analog_steps));
+  Serial.print("NO2 (a2): ");
+  Serial.println(a2_read * (max_volts / max_analog_steps));
+  
+
+  delay(3000);
+}
