85715b045e21df1345da01bf516ae192a32aab83
Axel <99491464+Axel3135@users.noreply.github.com>
2023-01-29 18:04:39 +0100

    IMU-data

---
+++ CanSatMPU_6050/CanSatMPU_6050.ino
@@ -0,0 +1,47 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+void setup(void) {
+	Serial.begin(9600);
+
+	// Try to initialize!
+	if (!mpu.begin()) {
+		Serial.println("Failed to find MPU6050 chip");
+		while (1) {
+		  delay(10);
+		}
+	}
+	Serial.println("MPU6050 Found!");
+
+	// set accelerometer range to +-8G
+	mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+	// set filter bandwidth to 21 Hz
+	mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+	delay(100);
+}
+
+void loop() {
+	/* Get new sensor events with the readings */
+	sensors_event_t a, g, temp;
+	mpu.getEvent(&a, &g, &temp);
+
+	/* Print out the values */
+	Serial.print("Acceleration X: ");
+	Serial.print(a.acceleration.x);
+	Serial.print(", Y: ");
+	Serial.print(a.acceleration.y);
+	Serial.print(", Z: ");
+	Serial.print(a.acceleration.z);
+	Serial.println(" m/s^2");
+  float totalAcceleration = sqrt(a.acceleration.x * a.acceleration.x + a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z);
+  Serial.println(totalAcceleration);
+  if (totalAcceleration < 2) {
+    Serial.println("Deploying parachute!");
+  }
+	Serial.println("");
+	delay(500);
+}
